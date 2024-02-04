e04af9a9902b4322a483b00e71e1945ce9c9df8a
FelipeKenji <143840233+FelipeKenji@users.noreply.github.com>
2024-02-04 13:48:53 -0300

    aula4

---
+++ aula04js/aula04.js
@@ -0,0 +1,87 @@
+/* 
+
+Operadores Matemáticos
+
+= É uma balança
++
+-
+/ divisão
+* vezes 
+% mod | mostra o resto da divisão ou seja 15/2 = 7.5 mas na conta sobra 1 faça essa conta no papel Felipe do futuro para vc entender melhor 
+++ Encrementar 1
+-- Decrementar 1
++= Encrementa mas eu escolho o número que encrementa ex: +=6
+-= Decrementa mas eu ecolho o número que decrementa ex: -=5
+*= Encrementa uma multiplicação ao número inicial
+/= mesma coisa da explicação de cima só que com vezes
+
+*/
+
+/*
+
+2 tipos diferentes de declarar e inicializar variáveis (É de extrema impotância iniciar as declarações colocando um valor nelas)
+
+let num1=0
+let num2=0
+
+let num1,num2=0 
+*/
+
+/* Se o "=10" estiver somente em um "num" o outro "num" não tera definição      
+
+let num1,num2=10  
+
+console.log(num1)
+console.log(num2)
+
+*/
+
+/* Eu também posso iniciar declarações da seguinte forma:
+
+let num1,num2,num3  num1=num2=num3=10  
+console.log(num1) 
+console.log(num2) 
+console.log(num3)
+
+dessa forma eu inicializo todas as variáveis em somente uma linha */
+
+
+/*
+let num1=0,num2=0,res=0
+num1=10
+num2=5
+res=num1 + num2
+console.log(res)  O resultado é 15 no console 
+*/
+
+
+/* Posso fazer operações também dentro do console.log() com por exemplo: console.log(num2-num1) no console aparecerá 5 */
+
+//Javascript utiliza as regras normais de matemática
+
+/*
+Demonstração do % no console apareceu 7.5(Resultado da divisão) e 1 (Resto da divisão)
+let num1=0,num2=0,res=0
+num1=2
+num2=15
+
+res=num2%num1
+
+console.log(num2/num1)
+console.log(num2%num1)
+*/
+
+
+/*
+++ Faz com que some 1 na variável ent no exemplo a baixo no console aparecerá 11 e assim por diante a cada vez que fizer "num1++" ele somará 1
+
+let num1=0,num2=0,res=0
+num1=10
+
+num1++
+
+console.log(num1)
+*/
+
+//A mesma coisa de cima vale para o "--" só que ele tira 1
+

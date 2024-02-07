1d69254f98bcf2816d27ccbc04b0a558568cbd28
FelipeKenji <143840233+FelipeKenji@users.noreply.github.com>
2024-02-07 20:23:24 -0300

    aula9

---
+++ aula09js/aula09.js
@@ -0,0 +1,30 @@
+/*
+CASO EU TENHA UMA STRING EM CONSOLE.LOG() E TIVER 2 VALORES SENDO  SOMADOS ELES IRÃO SE JUNTAR
+(O + vira um operador de concatenação)
+
+let n1=10
+let n2=20
+
+
+console.log(n1 + "" + n2)   console: 1020
+
+QUANDO A IMPRESSÃO FOR FEITA NO CONSOLE ELE SEMPRE LERÁ PRIMEIRO O ELEMENTO DA ESQUERDA DPS O DA DIREITA
+Pré-incremento: O valor é impresso junto com o incremento
+
+let n=10
+
+console.log(++n)  Console:11  
+
+Pós-incremento: O valor é impresso e depois ele incrementa
+
+let n=10
+
+console.log(n++)
+console.log(n)      Console: 10 e 11
+                             
+*/
+
+let n=10
+
+
+console.log(++n)

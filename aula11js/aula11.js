e40f4470c0cf8f2a41c913d995d9c5e40cba1633
FelipeKenji <143840233+FelipeKenji@users.noreply.github.com>
2024-02-07 22:09:31 -0300

    aula11

---
+++ aula11js/aula11.js
@@ -0,0 +1,11 @@
+//Elemento Typeof  retorna o tipo da variável
+
+let v1=10
+let v2="10"
+let v3=v1===v2
+let v4={nome:"Felipe"}
+
+console.log("Valor: " + v1 + " - tipo:" + typeof(v1))
+console.log("Valor: " + v1 + " - tipo:" + typeof(v2))
+console.log("Valor: " + v1 + " - tipo:" + typeof(v3))
+console.log("Valor: " + v1 + " - tipo:" + typeof(v4))

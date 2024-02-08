9327730d550d24731f4847462bb0a3c943e69700
FelipeKenji <143840233+FelipeKenji@users.noreply.github.com>
2024-02-07 21:52:41 -0300

    aula10

---
+++ aula10js/aula10.js
@@ -0,0 +1,36 @@
+// Operador ternário: ?
+
+//O operador ? te da formas mais interressantes de falar pro seu cliente se algo é true ou false
+
+/*
+Saber se um número é par ou impar
+
+let num=12
+let res=num%2
+
+if(res==0){
+    console.log("Par")
+}else{
+    console.log("Impar")
+}
+
+
+
+let num=10
+
+res=(!(num%2) ? "Par" : "Impar")
+
+console.log(res)
+
+Sintaxe da operação ternária: Teste lógico ? se verdadeiro : se falso
+
+*/
+
+//Utilizando operador ternário
+
+let st="A"
+
+res=(st == "A" ? "Ativo" : "Inativo")
+console.log(res)
+
+ 

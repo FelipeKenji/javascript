d996d10265be1c99beccd69d46c1ffb84dd8067e
Felipe Kenji <felipekanachiro@gmail.com>
2024-03-06 22:21:26 -0300

    aula 15

---
+++ aula15.js/aula15.js
@@ -0,0 +1,21 @@
+/* for(inicialização da variável de controle/contador; condição de execução[Avaliar se eu vou continuar executando meu loop ou não]; controle){
+
+Comandos que serão iterados nesse loop
+------------------------
+------------------------
+------------------------
+} 
+*/
+
+//Condição do for: Enquanto a condição for verdadeira ele itera se for falsa ele sai e continua a execução do programa
+console.log("Início do Programa")
+
+for(let i=0; i < 101; i++){
+ if(i%2==0){
+    console.log(i + " é par")
+ } else{
+    console.log(i + " é impar")
+ }
+}
+
+console.log("Fim do Programa")

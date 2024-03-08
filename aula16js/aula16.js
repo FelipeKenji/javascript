7821cea80555b2ef5b226897b458904d13201200
Felipe Kenji <felipekanachiro@gmail.com>
2024-03-08 10:29:18 -0300

    aula16

---
+++ aula16js/aula16.js
@@ -0,0 +1,21 @@
+//Utilizar for of para pegar os elementos
+//Utilizar for in para pegar as posições
+const objs=document.getElementsByTagName("div")
+
+let num=[10,20,30,40,50]
+
+for(o of objs){
+    console.log(o)
+}
+
+
+for(o in objs){
+    console.log(objs[n])
+}
+
+
+
+/*for(let i=0;i<num.length;i++){
+console.log(num[i])
+}
+*/

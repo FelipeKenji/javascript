b6f403a6fa500c86c0f3757227b268b2b1cb6c95
Felipe Kenji <felipekanachiro@gmail.com>
2024-03-08 10:39:49 -0300

    aula16

--- aula16js/aula16.js
+++ aula16js/aula16.js
@@ -5,12 +5,12 @@ const objs=document.getElementsByTagName("div")
 let num=[10,20,30,40,50]
 
 for(o of objs){
-    console.log(o)
+    console.log(o.innerHTML="Kenji")
 }
 
 
 for(o in objs){
-    console.log(objs[n])
+    console.log(o)
 }
 
 
